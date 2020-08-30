ee4b1c48ba7146a35dde0b067d5bee82fa0af3a5
trugamr <6trugamr9@gmail.com>
2020-08-30 13:53:55 +0530

    updated schema and resolvers to return companies

--- server/resolvers.js
+++ server/resolvers.js
@@ -4,11 +4,21 @@ const Query = {
     if (!job) throw new Error('Job not found.')
     return job
   },
-  jobs: (parent, args, { db }) => db.jobs.list()
+  jobs: (parent, args, { db }) => db.jobs.list(),
+  company: (parent, { id }, { db }) => {
+    const company = db.companies.get(id)
+    if (!company) throw new Error('Company not found.')
+    return company
+  }
 }
 
 const Job = {
   company: ({ companyId }, args, { db }) => db.companies.get(companyId)
 }
 
-module.exports = { Query, Job }
+const Company = {
+  jobs: (company, args, { db }) =>
+    db.jobs.list().filter(job => job.companyId === company.id)
+}
+
+module.exports = { Query, Job, Company }
